c9147c5e91fbd79e03e6f02e48e154453ba3aca4
Annaro8 <123467178+Annaro8@users.noreply.github.com>
2023-11-23 14:37:34 +0200

    Update 1 spele.py
    23.11.23.(pogas, opcijas)

--- 1 spele.py
+++ 1 spele.py
@@ -14,7 +14,7 @@ def btnClick(button):    #padod visu pogu
         count+=1    #palielina rūtiņu skaitu
         checkWinner()
     elif button["text"]=='' and speletajsX==False:
-       button["text"]='☆'
+       button["text"]='☆'   
        speletajsX=True
        count+=1 
        checkWinner()
@@ -23,17 +23,66 @@ def btnClick(button):    #padod visu pogu
     return
 
 
+
+galvenaizvele=Menu(mansLogs) #izveido galveno izvēli
+mansLogs.config(menu=galvenaizvele) #pievieno galvenam logam
+opcija=Menu(galvenaizvele,tearoff=False) #mazā izvēle
+galvenaizvele.add_cascade(label="Opcija",menu=opcija)
+
+opcija.add_command(label="Jauna spēle", command=reset)
+opcija.add_command(label="Iziet", command=mansLogs.quit)
+
+
+
+
+def disableButton():
+    btn1.config(state=DISABLED)
+    btn2.config(state=DISABLED)
+    btn3.config(state=DISABLED)
+    btn4.config(state=DISABLED)
+    btn5.config(state=DISABLED)
+    btn6.config(state=DISABLED)
+    btn7.config(state=DISABLED)
+    btn8.config(state=DISABLED)
+    btn9.config(state=DISABLED)
+    return 0
+
+def reset():
+    btn1.config(state=NORMAL)
+    btn2.config(state=NORMAL)
+    btn3.config(state=NORMAL)
+    btn4.config(state=NORMAL)
+    btn5.config(state=NORMAL)
+    btn6.config(state=NORMAL)
+    btn7.config(state=NORMAL)
+    btn8.config(state=NORMAL)
+    btn9.config(state=NORMAL)
+    btn1['text']=''
+    btn2['text']=''
+    btn3['text']=''
+    btn4['text']=''
+    btn5['text']=''
+    btn6['text']=''
+    btn7['text']=''
+    btn8['text']=''
+    btn9['text']=''
+    return 0
+
+
 def checkWinner():
     global checkWinner
     winner=False #ja  būs neizšķirts
     if(btn1["text"]=="❤" and btn2["text"]=="❤" and btn3["text"]=="❤" or btn4["text"]=="❤" and btn5["text"]=="❤" and btn6["text"]=="❤" or btn7["text"]=="❤" and btn8["text"]=="❤" and btn9["text"]=="❤" or btn1["text"]=="❤" and btn4["text"]=="❤" and btn7["text"]=="❤" or btn2["text"]=="❤" and btn5["text"]=="❤" and btn8["text"]=="❤" or btn3["text"]=="❤" and btn6["text"]=="❤" and btn9["text"]=="❤" or btn1["text"]=="❤" and btn5["text"]=="❤" and btn9["text"]=="❤" or btn3["text"]=="❤" and btn5["text"]=="❤" and btn7["text"]=="❤"):
         winner = True
         messagebox.showinfo("TicTacToe", "Speletajs ❤ ir uzvarētājs!!")
+        disableButton()
     elif(btn1["text"]=="☆" and btn2["text"]=="☆" and btn3["text"]=="☆" or btn4["text"]=="☆" and btn5["text"]=="☆" and btn6["text"]=="☆" or btn7["text"]=="☆" and btn8["text"]=="☆" and btn9["text"]=="☆" or btn1["text"]=="☆" and btn4["text"]=="☆" and btn7["text"]=="☆" or btn2["text"]=="☆" and btn5["text"]=="☆" and btn8["text"]=="☆" or btn3["text"]=="☆" and btn6["text"]=="☆" and btn9["text"]=="☆" or btn1["text"]=="☆" and btn5["text"]=="☆" and btn9["text"]=="☆" or btn3["text"]=="☆" and btn5["text"]=="☆" and btn7["text"]=="☆"):
         winner = True
         messagebox.showinfo("TicTacToe", "Spēlētājs ☆ ir uzvarētājs!!")
     elif count==9 and winner==False:
         messagebox.showinfo("TicTacToe", "Neizšķirts..")
+        disableButton()
+
 
 
 btn1=Button(mansLogs, text="", width=6, height=3, font=('Helvica',24),command=lambda:btnClick(btn1))
